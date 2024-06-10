a01f5a477b59e32f16c676e3d4707ba856dcbe83
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-06-10 18:37:55 +0530

    login page

--- Blog App/app.js
+++ Blog App/app.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const express = require('express')
 const expressLayout = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connectDB = require('./server/config/db')
 const app = express()
 const port = 3000 
@@ -10,6 +13,15 @@ const port = 3000
 
    app.use(express.urlencoded({ extended: true}));
    app.use(express.json());
+   app.use(cookieParser());
+   app.use(session({
+     secret: 'keyboard cat',
+     resave: false,
+     saveUninitialized: true,
+     store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI
+     })
+   }))
 
 app.use(express.static('public'));
 // Templating engine
@@ -18,6 +30,7 @@ app.set('layout','./layouts/main');
 app.set('view engine','ejs');
 
 app.use('/',require('./server/routes/main'));
+app.use('/',require('./server/routes/admin'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
