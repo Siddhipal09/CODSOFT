e699cfad02ee8bae956c82818e835062933346f2
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-07-01 02:05:24 +0530

    pending task problem solved

--- Project Management App/Frontend/src/pages/PendingTasks.jsx
+++ Project Management App/Frontend/src/pages/PendingTasks.jsx
@@ -14,7 +14,7 @@ const PendingTasks = () => {
       const data = await res.json();
       console.log('Fetched tasks:', data);
 
-      const pendingTasks = data.filter(task => task.status === 'pending');
+      const pendingTasks = data.filter(task => task.status === 'Pending');
       console.log('Filtered pending tasks:', pendingTasks);
         setTasks(pendingTasks);
         setError(null);
